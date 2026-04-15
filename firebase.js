2cbbd6e75f0531d94ef9c05317b3c42c0471c017
Timix249 <timurschule4@gmail.com>
2026-04-15 21:36:02 +0200

    Add Firebase configuration and initialization

--- firebase.js
+++ firebase.js
@@ -1,1 +1,10 @@
-// add your firebase config here
+const firebaseConfig = {
+  apiKey: "ВАШ_API_KEY",
+  authDomain: "ВАШ_PROJECT.firebaseapp.com",
+  projectId: "ВАШ_PROJECT"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
