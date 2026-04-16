e4b5e139ecd52af91225ca559195ce781e2a1c17
Timix249 <timurschule4@gmail.com>
2026-04-16 05:46:41 +0200

    Implement section display and data saving functions
    Added functions to show sections and save data to the database.

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-if ('serviceWorker' in navigator){navigator.serviceWorker.register('service-worker.js');}
+function show(id){
+  document.querySelectorAll("section").forEach(s=>s.classList.add("hidden"));
+  document.getElementById(id).classList.remove("hidden");
+}
+
+function save(){
+
+  db.collection("termine").add({
+    name: document.getElementById("name").value,
+    klasse: document.getElementById("klasse").value,
+    date: document.getElementById("date").value,
+    pause: document.getElementById("pause").value,
+    room: "Raum 008"
+  });
+
+  alert("Termin gespeichert!");
+}
+
+// PWA
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js");
+}
