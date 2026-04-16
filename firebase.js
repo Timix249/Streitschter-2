0e26ec648b83388e2f20bcaa2739940c6ce6304d
Timix249 <timurschule4@gmail.com>
2026-04-16 06:02:31 +0200

    Update Firebase configuration with new credentials

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 const firebaseConfig = {
-  apiKey: "ВАШ_API_KEY",
-  authDomain: "ВАШ_PROJECT.firebaseapp.com",
-  projectId: "ВАШ_PROJECT"
+  apiKey: "AIzaSyD_B7Kd4NGF585Y7RW3V4amkE1N-EGPL0g",
+  authDomain: "streitschlichter-9f634.firebaseapp.com",
+  projectId: "streitschlichter-9f634"
 };
 
 firebase.initializeApp(firebaseConfig);
